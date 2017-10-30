ab3a996e5b116a8a1e80766335d59d123a75ff77
Konstantin Kuchinskii <kokakokc@yandex.ru>
2017-10-30 14:24:05 +0300

    Improvements to Task 1,2

--- src/main/java/com/epam/jf/Kuchinskii/homework/Task1.java
+++ src/main/java/com/epam/jf/Kuchinskii/homework/Task1.java
@@ -1,12 +1,14 @@
-package main.java.com.jf.Kuchinskii.homework;
+package com.epam.jf.Kuchinskii.homework;
 
 public class Task1 {
     public static void main(String[] args) {
         String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate" };
         int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265 };
 
-        int first = times[0], second = 0;
-        int firstIndex = 0, secondIndex = 0;
+        int first = times[0];
+        int second = 0;
+        int firstIndex = 0;
+        int secondIndex = 0;
 
         for (int i = 0; i < times.length; i++) {
             if (times[i] < first)
