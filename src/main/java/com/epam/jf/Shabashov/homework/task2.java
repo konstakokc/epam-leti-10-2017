3aa4ca323e0cbb1ff09a8158de90e1c06c447ea0
NikitaShabashov <niksh81@gmail.com>
2017-11-02 18:24:31 +0300

    Fix variable names

--- src/main/java/com/epam/jf/Shabashov/homework/task2.java
+++ src/main/java/com/epam/jf/Shabashov/homework/task2.java
@@ -2,13 +2,13 @@ package com.epam.jf.Shabashov.homework;
 
 public class task2{
 	public static void main(String[] args) {
-		int n_min=0;
-		int n_max=0;
+		int nMin=0;
+		int nMax=0;
 		for (int i=0;i<args.length;i++) {
-			if (args[i].length()>=args[n_max].length()) n_max=i;
-			if (args[i].length()<=args[n_min].length()) n_min=i;
+			if (args[i].length()>=args[nMax].length()) nMax=i;
+			if (args[i].length()<=args[nMin].length()) nMin=i;
 		}
-		System.out.println("Minimal string: "+args[n_min]);
-		System.out.println("Maximal string: "+args[n_max]);
+		System.out.println("Minimal string: "+args[nMin]);
+		System.out.println("Maximal string: "+args[nMax]);
 	}
 }
