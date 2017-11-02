323d1642380b83ddaf8361912da0af4a7b5b9488
NikitaShabashov <niksh81@gmail.com>
2017-11-02 18:16:51 +0300

    Fix variables

--- src/main/java/com/epam/jf/Shabashov/homework/task1.java
+++ src/main/java/com/epam/jf/Shabashov/homework/task1.java
@@ -10,7 +10,8 @@ public class task1{
 	public static void main(String...args) {
 		String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate" };
 		int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265 };
-		int top1=0,top2=0;
+		int top1=0;
+		int top2=0;
 		for(int i=0;i<times.length;i++){
 			if (times[i]<times[top1]){
 				top2=i;
--- src/main/java/com/epam/jf/Shabashov/homework/task2.java
+++ src/main/java/com/epam/jf/Shabashov/homework/task2.java
@@ -2,7 +2,8 @@ package com.epam.jf.Shabashov.homework;
 
 public class task2{
 	public static void main(String[] args) {
-		int n_min=0,n_max=0;
+		int n_min=0;
+		int n_max=0;
 		for (int i=0;i<args.length;i++) {
 			if (args[i].length()>=args[n_max].length()) n_max=i;
 			if (args[i].length()<=args[n_min].length()) n_min=i;
