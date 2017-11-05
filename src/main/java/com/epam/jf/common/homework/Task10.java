ba63e8e8bab9a917f654bd228ffd4c8cff8db939
elefus <elefus@yandex.ru>
2017-11-06 01:54:05 +0300

    Добавлены условия 8-11 задач.

---
+++ src/main/java/com/epam/jf/common/homework/Task10.java
@@ -0,0 +1,11 @@
+package com.epam.jf.common.homework;
+
+public abstract class Task10 {
+
+    /**
+     * Вычисляет сумму цифр, составляющих указанное число с использованием рекурсии.
+     * @param value натуральное число.
+     * @return Сумма цифр, составляющих число.
+     */
+    public abstract long getSumDigits(long value);
+}
---
+++ src/main/java/com/epam/jf/common/homework/Task9.java
@@ -0,0 +1,11 @@
+package com.epam.jf.common.homework;
+
+public abstract class Task9 {
+
+    /**
+     * С помощью битовых и логических операций определяет, является ли указанное значение степенью двойки.
+     * @param value анализируемое не отрицательное число.
+     * @return true - число является степенью двойки, false в противном случае.
+     */
+    public abstract boolean isPowerOfTwo(long value);
+}
