218827ef96e6f405fb427f23a523d258dd27c65c
Konstantin Kuchinskii <kokakokc@yandex.ru>
2017-11-28 02:54:50 +0300

    Task 11 completed

--- src/main/java/com/epam/jf/kuchinskii/homework/Task11.java
+++ src/main/java/com/epam/jf/kuchinskii/homework/Task11.java
@@ -4,6 +4,13 @@ public class Task11 extends com.epam.jf.common.homework.Task11 {
 
     @Override
     public int reverse(int value) {
-        return (value < 10) ? value : (value % 10 + reverse(value / 10));
+        if (value > 999_999_999 || value < -999_999_999) {
+            throw new IllegalArgumentException("Value too big");
+        }
+        if (value < 0) {
+            value = Math.abs(value);
+        }
+        int digitsCount = (int) Math.log10(value);
+        return (value < 10) ? value : (value % 10) * (int)Math.pow(10, digitsCount) + reverse(value / 10);
     }
 }
--- src/test/java/com/epam/jf/kuchinskii/homework/Task11Test.java
+++ src/test/java/com/epam/jf/kuchinskii/homework/Task11Test.java
@@ -1,7 +1,7 @@
 package com.epam.jf.kuchinskii.homework;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -11,21 +11,22 @@ public class Task11Test {
     @Test
     void testZero(){
         Task11 tester = new Task11();
-        assertTrue(tester.reverse(0) == 0);
+        int result = tester.reverse(0);
+        assertEquals(result, 0);
     }
 
     @Test
     void testValue(){
         Task11 tester = new Task11();
         int result = tester.reverse(123456789);
-        assertTrue(result == 987654321);
+        assertEquals(result, 987654321);
     }
 
     @Test
     void testZeroesValue(){
         Task11 tester = new Task11();
         int result = tester.reverse(12345000);
-        assertTrue(result == 54321);
+        assertEquals(result, 54321);
     }
 
     @Test
