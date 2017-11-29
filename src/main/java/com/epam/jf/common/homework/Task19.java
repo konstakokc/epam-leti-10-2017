c92044c7d83910bd417474d4192534f3a70e21bc
elefus <elefus@yandex.ru>
2017-11-29 16:47:31 +0300

    Добавлено условие задач №18 и 19.

---
+++ src/main/java/com/epam/jf/common/homework/Task19.java
@@ -0,0 +1,11 @@
+package com.epam.jf.common.homework;
+
+public abstract class Task19 {
+
+    /**
+     * Поворачивает матрицу на 90 градусов против часовой стрелки.
+     * @param source Исходная матрица.
+     * @return Новая матрица, элементы которой получены путем поворота исходной.
+     */
+    public abstract int[][] rotateMatrixCounterclockwise(int[][] source);
+}
